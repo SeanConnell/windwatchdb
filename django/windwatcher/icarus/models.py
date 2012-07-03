94065c203590307a6e23bde85af3e9b3b5e28f08
Sean Connell <seanrobertconnell@gmail.com>
2012-07-02 21:28:07 -0700

    Added foreign key stuff to models, also figured out either python or django allows you to do Class(member_attrib_name=value) so all that init stuff was really unnecesary to begin with:

--- django/windwatcher/icarus/models.py
+++ django/windwatcher/icarus/models.py
@@ -3,7 +3,7 @@ from datetime import datetime as dt
 from datetime import timedelta as td
 
 """Holds the site coords for the NWDB lookup as well as name of site and a list of launches and landings"""
-class Site(models.Model):
+class Sites(models.Model):
     def __unicode__(self):
             return self.name
 
@@ -11,10 +11,20 @@ class Site(models.Model):
     lat = models.DecimalField(max_digits=12, decimal_places=6) #lat and lon for the point defining the site
     lon = models.DecimalField(max_digits=12, decimal_places=6)
 
-"""Holds wind direction in degrees azimuth, velocity (in mph), chance precipitation, temperature, cloudbase, and date of forecast."""
-class Weather(models.Model):    
+"""Holds a bunch of timeslice predictions for that day's weather"""
+class DayOfWeather(models.Model):
     def __unicode__(self):
-            return unicode(self.date_it_happens.strftime("%A, %d. %B %Y %I:%M%p"))
+        return unicode(self.date_it_happens.strftime("%A, %d. %B %Y %I:%M%p"))
+
+    date_it_happens = models.DateTimeField()
+    prediction_date = models.DateTimeField()
+    max_temperature = models.IntegerField() #future use
+    min_temperature = models.IntegerField() #future use
+
+"""A chunk of time for which weather has been predicted. Goes with the DayOfWeather to build out a forecast"""
+class WeatherTimeSlice(models.Model):
+    def __unicode__(self):
+        return unicode(self.start_time + self.duration)
 
     wind_direction = models.IntegerField(default=0)
     wind_speed = models.IntegerField(default=0)
@@ -22,12 +32,15 @@ class Weather(models.Model):
     temperature = models.IntegerField(default=0)
     apparent_temperature = models.IntegerField(default=0)
     cloud_cover_amount = models.IntegerField(default=0)
-    prediction_date = models.DateTimeField()
-    date_it_happens = models.DateTimeField()
+    day_of_occurance = models.ForeignKey(DayOfWeather)
 
 """ A launch and its sea level altitude, acceptable range of wind directions to fly in, and warnings about how to launch"""
-class Launch(models.Model):
+class Launches(models.Model):
+    def __unicode__(self):
+        return unicode(self.name + " launch for " + self.site.name)
+
     name = models.CharField(max_length=200)
+    site = models.ForeignKey(Sites)
     altitude = models.IntegerField(default=0)
     flyable_wind_direction_min = models.IntegerField(default=0)
     flyable_wind_direction_max = models.IntegerField(default=0)
@@ -36,8 +49,9 @@ class Launch(models.Model):
     launch_description = models.CharField(max_length=50000)
 
 """ A landing and its sea level altitude, and a description of how to do the approach"""    
-class Landing(models.Model):
+class Landings(models.Model):
     name = models.CharField(max_length=200)
+    site = models.ForeignKey(Sites)
     altitude = models.IntegerField(default=0)    
     flyable_wind_direction_min = models.IntegerField(default=0)
     flyable_wind_direction_max = models.IntegerField(default=0)
@@ -45,13 +59,8 @@ class Landing(models.Model):
     approach_warnings = models.CharField(max_length=50000)
     approach_description = models.CharField(max_length=50000)
 
-""" A queue of weather forecasts for a location every time division, uses weather object keys to lookup objects in database as needed. Looks up to the nearest time division by rounding start and end date times """
+"""All weather objects are collected into this queue for easy access"""
 class WeatherWatchQueue(models.Model):
-    sample_period = 3 # hours between database updates/list items    
-#class WeatherWatchQueue(models.Model):
-#    def __init__(self,start_date=dt.utcnow(),window=14,end_date=(dt.utcnow() +td(days=14) ) ):
-#        self.window = window
-#        self.start_date = start_date #default start date is now
-#        self.end_date = end_date #default end date is 14 days after now
-
-#    sample_period = 3 # hours between database updates/list items    
+    start_date = models.DateTimeField() 
+    end_date = models.DateTimeField() 
+    number_of_days = models.IntegerField() #current number of days there exists data for
