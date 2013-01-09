1a87d4ed26e18865758ba1dbe2a3c0fa468fcf5c
Sean Connell <seanrobertconnell@gmail.com>
2013-01-09 00:30:58 -0600

    Updating tests

--- icarus/tests/app_modules.py
+++ icarus/tests/app_modules.py
@@ -1,51 +1,8 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
 from icarus.models import Site,Launch,Landing,WeatherTimeSlice,DayOfWeather
 from icarus.compare_angles import compare_angles
 from icarus.compare_speed import compare_speed
 from icarus.join_dict import join_dict, add
-
-class LaunchTest(TestCase):
-    fixtures = ['icarus_models_test_data.json']
-
-    """
-    test_site = Site.objects.create(
-            name="Test Site One",
-            lat=45.476624,
-            lon=-123.787594,
-            city="Testcity",
-            state="TestState")
-
-    test_launch = Launch.objects.create(
-            name="Test Launch",
-            site=test_site,
-            flyable_wind_direction=180,
-            flyable_wind_direction_tolerance=45,
-            flyable_wind_speed=12,
-            flyable_wind_speed_tolerance=3)
-
-    test_wts = WeatherTimeSlice.objects.create(
-            start_time=datetime.now(),
-            day_of_occurance=None,
-            wind_direction=175,
-            wind_speed=9)
-    """
-
-
-    def test_check_tolerance(self):
-        pass
-
-    def test_check_wind_speed(self):
-        pass
-
-    def test_check_wind_direction(self):
-        pass
 
 class JoinDictTest(TestCase):
 
--- icarus/tests/models.py
+++ icarus/tests/models.py
@@ -1,53 +1,74 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
-from icarus.models import Site,Launch,Landing,WeatherTimeSlice,DayOfWeather
+from icarus.models import Site,Launch,Landing,WeatherTimeSlice,DayOfWeather,Ground
 
 class LaunchTest(TestCase):
+
     fixtures = ['icarus_models_test_data.json']
 
-    """
-    test_site = Site.objects.create(
-            name="Test Site One",
-            lat=45.476624,
-            lon=-123.787594,
-            city="Testcity",
-            state="TestState")
+    def setUp(self):
 
-    test_launch = Launch.objects.create(
-            name="Test Launch",
-            site=test_site,
-            flyable_wind_direction=180,
-            flyable_wind_direction_tolerance=45,
-            flyable_wind_speed=12,
-            flyable_wind_speed_tolerance=3)
-
-    test_wts = WeatherTimeSlice.objects.create(
-            start_time=datetime.now(),
-            day_of_occurance=None,
-            wind_direction=175,
-            wind_speed=9)
-    """
+        """test launch has these values:
+            fly_wspd: 12
+            wind_tol:  3
+            fly_wdir:180
+            wdir_tol: 45
+        """
+        self.test_launch = Launch.objects.get(pk=1)
+        self.test_wts = WeatherTimeSlice.objects.get(pk=34)
 
     def test_check_tolerance(self):
         pass
 
-    def test_check_wind_speed(self):
-        pass
+    def test_check_wind_speed_3(self):
+        self.test_wts.wind_speed = 12
+        test_flyability = self.test_launch.check_wind_speed(self.test_wts)
+        self.assertEqual(test_flyability, 3)
 
-    def test_check_wind_direction(self):
-        pass
+    def test_check_wind_speed_2(self):
+        self.test_wts.wind_speed = 8 
+        test_flyability = self.test_launch.check_wind_speed(self.test_wts)
+        self.assertEqual(test_flyability, 2)
+
+    def test_check_wind_speed_1(self):
+        self.test_wts.wind_speed = 5 
+        test_flyability = self.test_launch.check_wind_speed(self.test_wts)
+        self.assertEqual(test_flyability, 1)
+
+    def test_check_wind_speed_0(self):
+        self.test_wts.wind_speed = 2 
+        test_flyability = self.test_launch.check_wind_speed(self.test_wts)
+        self.assertEqual(test_flyability, 0)
+
+    def test_check_wind_direction_3(self):
+        self.test_wts.wind_direction = 180
+        test_flyability = self.test_launch.check_wind_dir(self.test_wts)
+        self.assertEqual(test_flyability, 3)
+
+    def test_check_wind_direction_2(self):
+        self.test_wts.wind_direction = 130
+        test_flyability = self.test_launch.check_wind_dir(self.test_wts)
+        self.assertEqual(test_flyability, 2)
+
+    def test_check_wind_direction_1(self):
+        self.test_wts.wind_direction = 70
+        test_flyability = self.test_launch.check_wind_dir(self.test_wts)
+        self.assertEqual(test_flyability, 1)
+
+    def test_check_wind_direction_0(self):
+        self.test_wts.wind_direction = 20 
+        test_flyability = self.test_launch.check_wind_dir(self.test_wts)
+        self.assertEqual(test_flyability, 0)
 
 class SiteTest(TestCase):
+
+    fixtures = ['icarus_models_test_data.json']
+
+    def setUp(self):
+        self.site1 = Site.objects.get(pk=1)
+        self.site2 = Site.objects.get(pk=2)
 
     "Found a bug where an empty flyability can happen, and the max function hates that"
     def test_empty_flyability_in_find_max_flyability(self):
         pass
 
     "TODO: Test all the function models for empty input args or things that will generate empty output args"
-
